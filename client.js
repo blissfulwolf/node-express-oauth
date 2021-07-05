1b34d451e978b82e94f851fbc1b7ee59b7d4d29d
Jasper Teo <cashgrid@yahoo.com>
2021-07-05 18:08:36 +0800

    4_Building the Client Application

--- client.js
+++ client.js
@@ -1,3 +1,4 @@
+const url = require("url")
 const express = require("express")
 const bodyParser = require("body-parser")
 const axios = require("axios").default
@@ -23,9 +24,74 @@ app.use(timeout)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+/* === Your code here === */
+
 /*
-Your code here
+4 = Building the Client Application | 45m
+1. Creating the authorization route
+2. Declaring the state
+3. Redirecting the user to the authorization endpoint
+4. Creating the callback endpoint
+5. Verifying the state
+6. Requesting for the access token
+7. Requesting user information
+8. Rendering the welcome page
 */
+
+app.get("/authorize", (req,res) => {
+
+	state = randomString()
+	const redirectUrl = url.parse(config.authorizationEndpoint)
+
+	redirectUrl.query = {
+		response_type: "code",
+		client_id: config.clientId,
+		redirect_uri: config.redirectUri,
+		scope: "permission:name permission:date_of_birth",
+		state: state,
+	}
+	res.redirect(url.format(redirectUrl))
+})
+
+app.get("/callback", (req,res) => {
+	if(state !== req.query.state) {
+		res.status(403).send("Error: Mismatch State")
+		return
+	}  
+
+		const { code } = req.query
+		axios({			
+				method:"POST",
+				url:config.tokenEndpoint,
+				auth: {
+					username:config.clientId,
+					password:config.clientSecret,
+				},
+				data: { code, },
+				validateStatus: null,
+		}) 	
+		.then((response) => {
+			return axios({
+				method: "GET",
+				url: config.userInfoEndpoint,
+				headers: {
+					authorization: "bearer " + response.data.access_token,
+				},
+			})
+		})
+		.then((response) => {
+			res.render("welcome", { user:response.data })
+		})
+		.catch((err) => {
+			console.error(err)
+			res.status(500).send("Error: something went wrong")
+		})
+
+	
+})
+
+/* === Your code here === */
+
 
 const server = app.listen(config.port, "localhost", function () {
 	var host = server.address().address
